1a8149c1916a0ae72ce3f8a8b053ea2e6d72c6bb
JDMTias <matiasiturbide@gmail.com>
2020-07-24 18:19:46 -0600

    frustrated

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -7,7 +8,7 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  // console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -21,11 +22,33 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+
+    console.log(colorToEdit.id)
+    
+    axiosWithAuth()
+    .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit )
+    .then((res) => {  
+      console.log(res)
+      console.log(res.data)
+    })
+    .catch((err) => console.log(err))
+
   };
 
-  const deleteColor = color => {
+  const deleteColor = (color) => {
+    console.log(color)
+    console.log(color.id)
     // make a delete request to delete this color
-  };
+    axiosWithAuth()
+    .delete(`/api/colors/${color.id}`)
+    .then((res) => {
+        console.log(res)
+        console.log(res.data)
+        // filter here
+      
+    })
+    .catch((err) => console.log(err))
+    }
 
   return (
     <div className="colors-wrap">
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
+
+const userCred = {
+  username:'Lambda School',
+  password:'i<3Lambd4'
+}
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials]=useState(userCred)
+
+  const handleChange = (e) => {
+     setCredentials({...credentials, [e.target.name]: e.target.value})
+  }
+
+  const getCredentials = e => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', userCred)
+    .then(res => {
+      console.log(res)
+      console.log(res.data.payload)
+      localStorage.setItem('token', res.data.payload)
+      window.location.assign("/colors")
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+          <div>
+              <form onSubmit={getCredentials}>
+                  <label className='srOnly'>Username:</label>
+                 <input
+                 type='text'
+                 name='username'
+                value='Lambda School'
+                 onChange={handleChange}
+                 />
+                  <label className='srOnly'>Password:</label>
+                 <input
+                 type='text'
+                 name='password'
+                value='i<3Lambd4'
+                 onChange={handleChange}
+                 />
+                 <button>Log in!</button>
+              </form>
+          </div>
   );
 };
 
